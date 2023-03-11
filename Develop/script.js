9fbe718ef6a24737add68b18376ccbc5a02b2326
joaquin <joaquin@barberis.com>
2023-03-10 22:14:04 -0500

    password lengh

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,20 @@
 // Assignment code here
+function generatePassword() {
+var passwordLength =  window.prompt("How many characters would you like your password to have? (Between 8-128 characters).")
 
+  return passwordLength; 
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var password = generatePassword();
+  var passwordField = document.querySelector("#password");
+
+  passwordField.value = password;
 
 }
 
