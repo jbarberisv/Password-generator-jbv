6613b663840483caa1750d00f6590d2895b0506d
joaquin <joaquin@barberis.com>
2023-03-13 16:58:27 -0400

    final product

--- Develop/script.js
+++ Develop/script.js
@@ -15,11 +15,12 @@ function length () {
   return passwordLength;
 }
 
-
+// make prompts & check answers
 function checkPrompt(question) {
   var notInRange = true;
   while (notInRange === true) {
-    var response =  window.prompt(question);
+    var response =  window.prompt(question); 
+    response = response.toUpperCase();
     if (response != "YES" && response != "NO") {
       window.alert("Enter YES or NO.");
       notInRange = true;
@@ -30,42 +31,45 @@ function checkPrompt(question) {
   }
 }
 
+// number randomizer with min - max
 function numberInterval(min, max) { 
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-
+// questions
 function generatePassword() {
-
   var passLength = length();
   var lowerCase = checkPrompt("Would you like for your password to have lowercase characters?");
   var upperCase = checkPrompt("Would you like for your password to have uppercase characters?");
   var numbers = checkPrompt("Would you like for your password to have numbers?");
   var specialCharacters= checkPrompt("Would you like for your password to have special charcters?");
-  console.log('lowerCase1', lowerCase);
+  var password = '';
+
+//random ASCII queries dpeending on the digit
   for (var num = 0; num < passLength; num++) {
-    console.log('lowerCase', lowerCase);
-
     if (lowerCase === 'YES') {
-      // encotrar un caracter entre el 97 y el 122
-      // random 
-
       var randomNum = numberInterval(97, 122);
       var randomChar = String.fromCharCode(randomNum);
-      console.log (randomChar)
-
+      password = password + randomChar;
     }
-    
+    if (upperCase === 'YES') {
+      var randomNum = numberInterval(65, 90);
+      var randomChar = String.fromCharCode(randomNum);
+      password = password + randomChar;
+    }
+    if (numbers === 'YES') {
+      var randomNum = numberInterval(48, 57);
+      var randomChar = String.fromCharCode(randomNum);
+      password = password + randomChar;
+      
+    }
+    if (specialCharacters === 'YES') {
+      var randomNum = numberInterval(33, 47);
+      var randomChar = String.fromCharCode(randomNum);
+      password = password + randomChar;
+    }
   }
-
-  
-  // Generate a random number between 0 and 255
-
-  // Convert the random number to its corresponding ASCII character
-  // var randomChar = String.fromCharCode(randomNum);
-
-
-  
+  return password
 }
 
 // Get references to the #generate element
